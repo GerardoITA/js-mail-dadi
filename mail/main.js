b5b268158f436f06bc69c5ed5e17a98aa6516fbf
GerardoITA <gerardo.giusti@tiscali.it>
2022-11-10 15:55:55 +0100

    layout fatto, lavorando su js

---
+++ mail/main.js
@@ -0,0 +1,17 @@
+// Definisco button
+const myButton = document.getElementById("button")
+// Definisco array contenente email che hanno accesso al "documento"
+const myArray = ["pepethefrog@gmail.com", "mario.draghi@libero.it", "pope.francis.1936@vatican.com", "jesuschristnotsuperstar@heaven.srl.com", "LETMEIN@letmein.letmein"]
+
+// Inserisco funzione del bottone
+myButton.addEventListener(click,
+    function() {
+        let myEmail = document.getElementById.value("email");
+
+        // 
+        if (myEmail ==)
+
+    }
+
+
+)
